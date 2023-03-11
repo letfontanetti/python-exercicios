245a570c1c59342c692a347ff1187eb78cfa4d79
letfontanetti <124930034+letfontanetti@users.noreply.github.com>
2023-03-11 16:57:49 -0300

    python

---
+++ MaiorMenor.py
@@ -0,0 +1,10 @@
+print("Olá, mundo!")
+
+num = float(input("Informe o numero: "))
+
+if(num > 0):
+    print("Número maior que zero ")
+elif(num == 0):
+    print("Zero ")
+else:
+    print("Número menor que zero ")
---
+++ vogal.py
@@ -0,0 +1,8 @@
+print("Olá, mundo!")
+
+letra = input("Informe a letra: ")
+
+if(letra == "a" or letra == "e" or letra == "i" or letra == "o" or letra == "u"):
+    print("Sua letra é uma vogal")
+else:
+    print("Sua letra é uma consoante")
